4bd7971edf835eeecf7092cc228717c8ff878cd3
NguyennamsonBTH <namson255@gmail.com>
2023-01-04 08:04:22 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NguyenNamSon515.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
